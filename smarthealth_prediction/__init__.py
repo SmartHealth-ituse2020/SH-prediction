549eaed06d24bdce79c0595eec52303f266eac69
Emek Gözlüklü <gozluklu16@itu.edu.tr>
2021-01-15 21:54:06 +0300

    add allowed ips

--- smarthealth_prediction/__init__.py
+++ smarthealth_prediction/__init__.py
@@ -15,11 +15,20 @@ def create_app(cfg="DEV"):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
 
+    ALLOWED_DOMAINS = [
+        "52.214.149.142",
+        "54.77.113.178",
+        "52.50.35.172",
+        "34.252.129.97",
+        "34.251.140.205",
+        "52.50.189.174",
+        "52.213.192.18",
+        "34.250.41.57",
+    ]
     @app.before_request
     def limit_remote_addr():
-        if request.remote_addr != '127.0.0.1':  # != web app ip
+        if request.remote_addr not in ALLOWED_DOMAINS:  # != web app ip
             abort(403)  # Forbidden
 
     app.add_url_rule("/predict", view_func=predict.predict)
     return app
-
